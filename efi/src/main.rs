72926eee3e8830e8715ae48a3803d4fb291a57da
SalsaGal <fielddashos@gmail.com>
2022-09-05 16:07:16 +1000

    Only make text_buffer when needed

--- efi/src/main.rs
+++ efi/src/main.rs
@@ -9,9 +9,9 @@ fn main(_handle: Handle, mut system_table: SystemTable<Boot>) -> Status {
     uefi_services::init(&mut system_table).unwrap();
     system_table.stdout().clear().unwrap();
 
-    let mut text_buffer = [0; 80];
     macro_rules! print {
         ($text: expr) => {
+            let mut text_buffer = [0; $text.len()];
             let buf = CStr16::from_str_with_buf($text, &mut text_buffer).unwrap();
             system_table.stdout().output_string(&buf).unwrap();
         };
