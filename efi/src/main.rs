60ecc4292a06c4c9ea72fa60a92e612fa4836110
SalsaGal <fielddashos@gmail.com>
2022-09-05 16:05:28 +1000

    Add print macro

--- efi/src/main.rs
+++ efi/src/main.rs
@@ -7,11 +7,18 @@ use uefi::{prelude::*, CStr16};
 #[entry]
 fn main(_handle: Handle, mut system_table: SystemTable<Boot>) -> Status {
     uefi_services::init(&mut system_table).unwrap();
-
-    let mut buf = [0; 16];
-    let buf = CStr16::from_str_with_buf("Hello, World!", &mut buf).unwrap();
     system_table.stdout().clear().unwrap();
-    system_table.stdout().output_string(&buf).unwrap();
+
+    let mut text_buffer = [0; 80];
+    macro_rules! print {
+        ($text: expr) => {
+            let buf = CStr16::from_str_with_buf($text, &mut text_buffer).unwrap();
+            system_table.stdout().output_string(&buf).unwrap();
+        };
+    }
+
+    print!("Test");
+
     loop {}
 
     Status::SUCCESS
