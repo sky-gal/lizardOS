0deb5fd90ea852e2ac68b54559866cd1be705fed
SalsaGal <fielddashos@gmail.com>
2022-09-05 16:59:04 +1000

    Start writing to serial

--- efi/src/main.rs
+++ efi/src/main.rs
@@ -2,11 +2,30 @@
 #![no_std]
 #![feature(abi_efiapi)]
 
-use uefi::{prelude::*, CStr16};
+use uefi::{
+    data_types::Identify,
+    prelude::*,
+    proto::console::serial::Serial,
+    table::boot::{OpenProtocolAttributes, OpenProtocolParams},
+    CStr16,
+};
 
 #[entry]
-fn main(_handle: Handle, mut system_table: SystemTable<Boot>) -> Status {
+fn main(handle: Handle, mut system_table: SystemTable<Boot>) -> Status {
     uefi_services::init(&mut system_table).unwrap();
+
+    let serial = system_table.boot_services().open_protocol::<Serial>(
+        OpenProtocolParams {
+            handle: system_table
+                .boot_services()
+                .locate_handle_buffer(uefi::table::boot::SearchType::ByProtocol(&Serial::GUID))
+                .unwrap()
+                .handles()[0],
+            agent: handle,
+            controller: None,
+        },
+        OpenProtocolAttributes::GetProtocol,
+    ).unwrap().interface.get();
 
     macro_rules! print {
         ($text: expr) => {
@@ -25,7 +44,7 @@ fn main(_handle: Handle, mut system_table: SystemTable<Boot>) -> Status {
     clear!();
     print!("Test");
 
-    loop {}
+    unsafe { Serial::write(&mut *serial, "serial".as_bytes()).unwrap(); }
 
-    Status::SUCCESS
+    loop {}
 }
