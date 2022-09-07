ed594d2eef93c6b5f72c5827864801f3257910d1
SalsaGal <fielddashos@gmail.com>
2022-09-07 11:25:54 +1000

    Remove serial

--- efi/src/main.rs
+++ efi/src/main.rs
@@ -3,29 +3,13 @@
 #![feature(abi_efiapi)]
 
 use uefi::{
-    data_types::Identify,
     prelude::*,
-    proto::console::serial::Serial,
-    table::boot::{OpenProtocolAttributes, OpenProtocolParams},
     CStr16,
 };
 
 #[entry]
-fn main(handle: Handle, mut system_table: SystemTable<Boot>) -> Status {
+fn main(_handle: Handle, mut system_table: SystemTable<Boot>) -> Status {
     uefi_services::init(&mut system_table).unwrap();
-
-    let serial = system_table.boot_services().open_protocol::<Serial>(
-        OpenProtocolParams {
-            handle: system_table
-                .boot_services()
-                .locate_handle_buffer(uefi::table::boot::SearchType::ByProtocol(&Serial::GUID))
-                .unwrap()
-                .handles()[0],
-            agent: handle,
-            controller: None,
-        },
-        OpenProtocolAttributes::GetProtocol,
-    ).unwrap().interface.get();
 
     macro_rules! print {
         ($text: expr) => {
@@ -42,9 +26,7 @@ fn main(handle: Handle, mut system_table: SystemTable<Boot>) -> Status {
     }
 
     clear!();
-    print!("Test");
-
-    unsafe { Serial::write(&mut *serial, "serial".as_bytes()).unwrap(); }
+    print!("Starting LizardOS");
 
     loop {}
 }
